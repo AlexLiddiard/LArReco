51300581a452abd28a2dd550bf1aee5cf0a35e72
Jack Collings <phuznm@eppgpu01.epp.warwick.ac.uk>
2019-10-23 16:40:28 +0100

    True MC particles added to information written to the ROOT file, and modified python root file read to display graph.

--- ReadPfoTree.py
+++ ReadPfoTree.py
@@ -2,4 +2,7 @@ import ROOT
 f = ROOT.TFile.Open("PFOs.root", "read")
 t = f.Get("PFO")
 t.Print()
-t.Scan("EventId:PfoId:ParentPfoId:DaughterPfoIds:HierarchyTier:Vertex:DriftCoordU:WireCoordU:ElectromagneticEnergyU:HadronicEnergyU")
+#t.Scan("EventId:PfoId:ParentPfoId:DaughterPfoIds:HierarchyTier:Vertex:DriftCoordW:WireCoordW:ElectromagneticEnergyW:HadronicEnergyW")
+t.GetEntryWithIndex(0, 1)
+t.Draw("DriftCoordU:WireCoordU", "PfoId == 1 && EventId == 0")
+input()
