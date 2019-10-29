860d18744b9c2099bd03cfa4eb833f24d6284e6b
Alex Liddiard <alexliddiard@localhost.localdomain>
2019-10-29 09:35:12 +0000

    Improved algorithm by rotating the coordinates based on OLS linear fit. This makes all tracks roughly paralel with the x axis, and the bins are counted only along this axis.

--- ROOT_TreeDataAnalysis.py
+++ ROOT_TreeDataAnalysis.py
@@ -1,13 +1,14 @@
 # This function is used to transcribe the data from the ROOT file stored as vectors into python arrays.
 import ROOT
 import statistics
+import math
 
 # Algorithm parameters
-binWidth = 0.3
-minBins = 10
-stdThresholdU = 1.00
-stdThresholdV = 0.70
-stdThresholdW = 0.70
+binWidth = 0.45
+minBins = 2
+stdThresholdU = 0.55
+stdThresholdV = 0.55
+stdThresholdW = 0.55
 
 def ReadROOTVector(ROOTVector):
 	pyVector = []
@@ -80,18 +81,34 @@ def GetBinCounts(numbers, binWidth):
 
 def ShowerInView(driftCoord, wireCoord, stdThreshold):
 	# Get bin counts for each of the coordinate components of the W plane hits
-	driftBins = GetBinCounts(driftCoord, binWidth)
-	wireBins = GetBinCounts(wireCoord, binWidth)
+	#driftBins = GetBinCounts(driftCoord, binWidth)
+	#wireBins = GetBinCounts(wireCoord, binWidth)
 
-	if (min(len(driftBins), len(wireBins)) < minBins):		# Make sure there are enough bin counts
-		return (-1, 0)
+	b = OLS(driftCoord, wireCoord)[0]
+	driftCoordRotated = RotateClockwise(driftCoord, wireCoord, b)[0] # Rotate the coords so that any tracks will lie roughly parallel to the x axis.
+	rotatedBins = GetBinCounts(driftCoordRotated, binWidth)
+
+	# Make sure there are enough bin counts
+	#driftStd = float('inf')
+	#wireStd = float('inf')
+	#rotatedStd = float('inf')
+	#if len(driftBins) >= minBins:
+	#	driftStd = statistics.stdev(driftBins)
+	#if len(wireBins) >= minBins:
+	#	wireStd = statistics.stdev(wireBins)
+	if len(rotatedBins) >= minBins:
+		rotatedStd = statistics.stdev(rotatedBins)
+	else:
+		return -1, 0						# Not enough bins
 
 	# Calculate the bin count standard deviations, take the smallest result
-	stdMin = min(statistics.stdev(driftBins), statistics.stdev(wireBins))
-	if (stdMin > stdThreshold): 					# If it is large enough the PFO is a (potential) shower
-		return (1, stdMin)
+	#stdMin = min(driftStd, wireStd, rotatedStd)
+	#if stdMin == float('inf'):					# Not enough bins
+	#	return -1, 0
+	if rotatedStd > stdThreshold: #stdMin > stdThreshold: 		# If it is large enough the PFO is a (potential) shower
+		return 1, rotatedStd #stdMin
 	else:								# Otherwise it is a track
-		return (0, stdMin)
+		return 0, rotatedStd #stdMin
 
 def TrackShowerAlg(pfo):
 	showerStats = []
@@ -110,6 +127,52 @@ def TrackShowerAlg(pfo):
 	else: # Not enough info to decide
 		return -1
 
+# Ordinary Least Squares line fit
+def OLS(xCoords, yCoords):
+	Sxy = 0
+	Sxx = 0
+	Sx = 0
+	Sy = 0
+	n = len(xCoords)
+	if n == 0:
+		return float('inf'), 0
+
+	for i in range(0, n):
+		Sxy += xCoords[i] * yCoords[i]
+		Sxx += xCoords[i] * xCoords[i]
+		Sx += xCoords[i]
+		Sy += yCoords[i]
+	
+	if (Sx == 0 and Sxx == 0):
+		print(xCoords)
+		print(yCoords)
+	#print("Sxy:%.3f Sxx:%.3f Sx:%.3f Sy:%.3f" % (Sxy, Sxx, Sx, Sy), end = " ")
+	divisor = Sxx - Sx * Sx / n
+	
+	if divisor == 0:
+		return (float('inf'), 0)
+	b = (Sxy - Sx * Sy / n) / divisor
+	a = Sy / n - b * Sx / n
+	return b, a
+
+# Rotate a set of points clockwise by angle theta, note that tanTheta = tan(theta) = gradient
+def RotateClockwise(xCoords, yCoords, tanTheta):
+	cosTheta = 1 / math.sqrt(1 + tanTheta * tanTheta)
+	sinTheta = tanTheta * cosTheta
+	xCoordsNew = []
+	yCoordsNew = []
+	for i in range(0, len(xCoords)):
+		xCoordsNew.append(xCoords[i] * cosTheta + yCoords[i] * sinTheta)
+		yCoordsNew.append(yCoords[i] * cosTheta - xCoords[i] * sinTheta)
+	return xCoordsNew, yCoordsNew
+
+
+def TrulyIsShower(pfo):
+	absPDGs = [abs(pfo.monteCarloPDGU), abs(pfo.monteCarloPDGV), abs(pfo.monteCarloPDGW)]
+	if absPDGs.count(11) > 1:
+		return 1
+	else:
+		return 0
 
 f = ROOT.TFile.Open(input("Root file name/path: "), "read")
 events = []		# Array containing arrays of Pfos from the same event.
@@ -129,16 +192,26 @@ for pfo in f.PFOs:
 # The for loop does not append the event to the array
 eventPfos.append(eventPfos)
 
+score = 0
+count = 0
 for eventPfos in events:
 	for pfo in eventPfos:
-		if pfo.pfoId == 0:
+		if pfo.pfoId == 0 or TrulyIsShower(pfo) == 1:
 			continue
+		count += 1
 		print(str(pfo), end = " ")
 		result = TrackShowerAlg(pfo)
 		if result == 1:
-			print("Result: Shower")
+			print("Result: S", end=" ")
 		elif result == 0:
-			print("Result: Track")
+			print("Result: T", end=" ")
 		else:
-			print("Result: Unknown")
-
+			print("Result: U")
+		
+		if result != -1:
+			if result == TrulyIsShower(pfo):
+				print("Correct")
+				score += 1
+			else:
+				print("Incorrect")
+print("Accuracy: " + str(100 * score / count) + "%")
