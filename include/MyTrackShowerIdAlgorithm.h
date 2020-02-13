8c3b77b488d9922db0fa879af433cbd2eb75f6bc
Alex Liddiard <phunqq@godzilla.csc.warwick.ac.uk>
2020-02-13 11:10:24 +0000

    Changed some integers to unsigned integers.

--- include/MyTrackShowerIdAlgorithm.h
+++ include/MyTrackShowerIdAlgorithm.h
@@ -54,7 +54,7 @@ private:
     pandora::StatusCode Run();
     pandora::StatusCode ReadSettings(const pandora::TiXmlHandle xmlHandle);
 
-    int WritePfo(const pandora::ParticleFlowObject *const pPfo, const int pfoId = 0, const int parentPfoId = -1, const int hierarchyTier = 0);
+    unsigned int WritePfo(const pandora::ParticleFlowObject *const pPfo, const unsigned int pfoId = 0, const int parentPfoId = -1, const unsigned int hierarchyTier = 0);
     void GetCaloHitInfo(const pandora::ParticleFlowObject *const pPfo, pandora::HitType hitType, ViewHits &viewHits);
     std::string GetFileName(const std::string& filePath);
     void GetIncidentMCPs(const pandora::MCParticleList *const pMCParticleList, pandora::MCParticleList &parentMCNuList);
@@ -62,18 +62,18 @@ private:
         const lar_content::LArMCParticleHelper::MCContributionMap &basicMap, 
         const pandora::MCParticle *const pMCParticle, 
         const bool isShowerProduct, 
-        const int hierarchyTier, 
+        const unsigned int hierarchyTier, 
         pandora::CaloHitList &caloHitsToMerge, 
         lar_content::LArMCParticleHelper::MCContributionMap &selectiveMap);
     void GetBestMatchedMCParticleInfo(const pandora::ParticleFlowObject *const pPfo, ViewHits &UView, ViewHits &VView, ViewHits &WView);
-    void PrintMCParticles(const lar_content::LArMCParticleHelper::MCContributionMap &mcContributionMap, const int minHits = 1) const;
-    void PrintMCParticle(const pandora::MCParticle *const pMCParticle, const lar_content::LArMCParticleHelper::MCContributionMap &mcToTrueHitListMap, const int depth = 0, const int minHits = 1, bool printDaughters = true) const;
+    void PrintMCParticles(const lar_content::LArMCParticleHelper::MCContributionMap &mcContributionMap, const unsigned int minHits = 1) const;
+    void PrintMCParticle(const pandora::MCParticle *const pMCParticle, const lar_content::LArMCParticleHelper::MCContributionMap &mcToTrueHitListMap, const unsigned int depth = 0, const unsigned int minHits = 1, bool printDaughters = true) const;
     void GetPfoToHitsMap(const pandora::PfoList &pPfoList, lar_content::LArMCParticleHelper::PfoContributionMap &pfoToHitsMap);
 
     // Member variables here
     std::string     m_caloHitListName;          ///< Name of input calo hit list
     std::string     m_mcParticleListName;       ///< Name of input MC particle list
-    int             m_mcMappingMinHits;          ///< Minimum number of hits for a MC particle to be mapped
+    unsigned int             m_mcMappingMinHits;          ///< Minimum number of hits for a MC particle to be mapped
 
     std::string		m_treeName; 		        ///< Name of output tree
     std::string		m_fileName; 		        ///< Name of output file
@@ -85,9 +85,9 @@ private:
     const pandora::MCParticle *m_incidentMcp;
 
     // PFO tree variables
-    int					m_EventId;		    ///< Current event id
-    int					m_HierarchyTier;	///< PFO hierarchy tier
-    int					m_PfoId;		    ///< Current PFO id
+    unsigned int					m_EventId;		    ///< Current event id
+    unsigned int					m_HierarchyTier;	///< PFO hierarchy tier
+    unsigned int					m_PfoId;		    ///< Current PFO id
     int					m_ParentPfoId;		///< Parent PFO id
     pandora::IntVector	*m_pDaughterPfoIds;	///< Daughter PFO ids
     float				m_Vertex[3];		///< PFO interaction vertex
@@ -95,10 +95,10 @@ private:
     ViewHits			m_VViewHits;		///< V view calo hits
     ViewHits			m_WViewHits;		///< W view calo hits
     ViewHits			m_ThreeDViewHits;	///< 3D view calo hits
-    int                 m_mcNuanceCode;     ///< Interaction type
+    unsigned int                 m_mcNuanceCode;     ///< Interaction type
     int					m_mcPdgCode;		///< truth particle for this PFO
     float               m_mcpMomentum;      ///< truth particle momentum
-    int                 m_mcHierarchyTier;  ///< truth particle hierarchy tier
+    unsigned int                 m_mcHierarchyTier;  ///< truth particle hierarchy tier
 };
 
 //------------------------------------------------------------------------------------------------------------------------------------------
